94a927cb2f05ef74958910a3d5ca082abfa19994
Berdic Nikola <berdicnikola@gmail.com>
2016-10-24 22:11:32 +0200

    Added models.

--- Project/ObjectMatching/ObjectMatching/ObjectMatching.py
+++ Project/ObjectMatching/ObjectMatching/ObjectMatching.py
@@ -1,21 +1,9 @@
-from Scripts import non_rigid_registration as nrr
+#from Scripts import non_rigid_registration as nrr
 import trimesh as tm
-import meshpy
 #import vtk_visualizer
 
-mesh = tm.load_mesh("D:/bunny.obj");
-mesh.apply_transform([[1,0,0,2],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
-mesh2 = tm.load_mesh("D:/bunny.obj");
+mesh = tm.load_mesh("D:/Repositories/ObjectMatching/Project/ObjectMatching/ObjectMatching/models/sphere.obj");
+#mesh.apply_transform([[1,0,0,2],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+#mesh2 = tm.load_mesh("models/pyramid.obj");
 
-
-
-lm.vertices = [1, 1, 1]
-
-#mesh3 = mesh + mesh2;
-#mesh3.show();
-
-#mesh.vertices.show()
-#print(mesh.vertices)
-#nrr.non_rigid_registration(mesh.vertices,mesh2.vertices#
-
-#vtk_visualizer.plot3d.plotxyz(np.array(mesh2.vertices))
+mesh.show()
